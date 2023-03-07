e5e4632835f40a6124c8a1e882ee0c9f89af3dce
arslanyusuf <mynameisarsi@gmail.com>
2023-03-07 15:45:52 +0400

    added router module import

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
-  imports: [BrowserModule, SharedModule],
+  imports: [BrowserModule, RouterModule, SharedModule],
   providers: [],
   exports: [HeaderComponent, FooterComponent],
 })
